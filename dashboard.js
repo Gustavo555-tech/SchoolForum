1961874bd1d394514acc5a2252e16528ce192ed4
Gustavo555-tech <gustavo.barbosa1@icloud.com>
2023-12-21 23:29:06 +0100

    "u"

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,10 @@
+// Inside your dashboard.js file
 document.addEventListener('DOMContentLoaded', function () {
     var profilePicture = document.getElementById('profile-picture');
     var menuContainer = document.getElementById('menu-container');
 
-    // Voeg een click event listener toe aan de profielfoto
     profilePicture.addEventListener('click', function () {
-        // Toggle de 'hidden' klasse van het menu-container om het te tonen/verbergen
+        // Toggle the 'hidden' class of the menu-container to show/hide it
         menuContainer.classList.toggle('hidden');
     });
 });
