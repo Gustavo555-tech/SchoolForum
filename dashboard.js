d1dbd63d17840c20358e6ed863ec68778a4765d2
Gustavo555-tech <gustavo.barbosa1@icloud.com>
2023-12-21 23:34:51 +0100

    "u"

--- dashboard.js
+++ dashboard.js
@@ -3,8 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var profilePicture = document.getElementById('profile-picture');
     var menuContainer = document.getElementById('menu-container');
 
-    profilePicture.addEventListener('click', function () {
+    profilePicture.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent the click event from reaching the document
+
         // Toggle the 'hidden' class of the menu-slate to show/hide it
-        document.querySelector('.menu-slate').classList.toggle('hidden');
+        var menuSlate = document.querySelector('.menu-slate');
+        menuSlate.classList.toggle('hidden');
+
+        // Close the menu if clicking outside
+        document.addEventListener('click', function closeMenu(event) {
+            if (!menuSlate.contains(event.target) && !profilePicture.contains(event.target)) {
+                menuSlate.classList.add('hidden');
+                document.removeEventListener('click', closeMenu);
+            }
+        });
     });
 });
